0b9131b0f470cd428368c11c1c276663a8b7991a
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-10-11 09:37:43 +0300

    first commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Sümeyye Kılıçoğlu on 7.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    init(simpsonName : String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+   
+}
+ 
